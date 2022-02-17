01af257ddaca12e151683633aba5a7615742e03f
MahimaAsthana <mahimaasthana@gmail.com>
2022-02-17 17:26:25 +0530

    commit

---
+++ config.js
@@ -0,0 +1,13 @@
+
+
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAh_tcl1uBG72zmb3q-vOO6U7o6U4M6ueQ",
+  authDomain: "spectagram-a0794.firebaseapp.com",
+  projectId: "spectagram-a0794",
+  storageBucket: "spectagram-a0794.appspot.com",
+  messagingSenderId: "392997771167",
+  appId: "1:392997771167:web:6e2b89287b8594da7c8faa"
+};
+
+
